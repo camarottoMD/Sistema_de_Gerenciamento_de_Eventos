c66fcea41bc353537b37e35642ef4a0a1d5bfd08
CmD_! <matheus.domingues2@estudante.ifms.edu.br>
2025-06-08 22:22:57 -0300

    mesma coisa no utlimo commit, só preciso manipular as data melhor

--- add_evento.py
+++ add_evento.py
@@ -4,18 +4,19 @@ eventos = {}
 
 def adicionar_evento():
     nome_evento = input("Digite o nome do evento: ")
-    data = input("Digite a data do evento: ")
+    data = input("Digite a data do evento (dd/mm/aaaa): ")
     tema = input("Digite o tema do evento: ")
 
     nome_evento = (
         nome_evento.strip().title()
     )  # Remove espaços no fim e começo e coloca a primeira letra em maiúsculo
 
+    #id de evento
+    
     tema = tema.strip().title()
-
     data = datetime.strptime(
         data,
-        "%d,%m,%Y"
+        "%d/%m/%Y" #-> so a aceita nesse formato
     )  # strptime = string → data / strftime = data → string    #   $d->dia em numeral     %m-> mes em numeral     %Y-> ano completo em numeral
 
     eventos[nome_evento] = {
